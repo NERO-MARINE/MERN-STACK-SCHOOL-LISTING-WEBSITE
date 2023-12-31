7982130a226065e87886f384765a2dd76e370168
Oghenero Okerri <oghenerokerri@gmail.com>
2023-12-31 16:18:09 +0100

    switched to nodejs native cors

--- api/index.js
+++ api/index.js
@@ -7,20 +7,49 @@ const schoolRoutes = require("./routes/schoolRoutes");
 const authRoutes = require("./routes/authRoutes");
 const forgotPswRoute = require("./routes/forgotPswRoute");
 const cookieParser = require("cookie-parser");
-const cors = require("cors");
+// const cors = require("cors");
 
 const app = express();
-const corsOptions = {
-  origin: ["http://localhost:3000", "https://naijaschoolsearch.onrender.com"],
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true, // Enable credentials (cookies, authorization headers, etc.)
-  optionsSuccessStatus: 204, // Some legacy browsers (IE11, various SmartTVs) choke on 204
-};
+// const corsOptions = {
+//   origin: ["http://localhost:3000", "https://naijaschoolsearch.onrender.com"],
+//   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   credentials: true, // Enable credentials (cookies, authorization headers, etc.)
+//   optionsSuccessStatus: 204, // Some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 // Handle preflight requests
-app.options("*", cors(corsOptions));
+// app.options("*", cors(corsOptions));
+
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    "http://localhost:3000",
+    "https://naijaschoolsearch.onrender.com",
+  ];
+
+  // Only allow requests from the specified origins
+  if (allowedOrigins.includes(req.headers.origin)) {
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
+  }
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS,CONNECT,TRACE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, X-Content-Type-Options, Accept, X-Requested-With, Origin, Access-Control-Request-Method, Access-Control-Request-Headers"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Private-Network", true);
+  // Firefox caps this at 24 hours (86400 seconds).
+  // Chromium (starting in v76) caps at 2 hours (7200 seconds).
+  // The default value is 5 seconds.
+  res.setHeader("Access-Control-Max-Age", 7200);
+
+  next();
+});
 
 app.use(cookieParser());
 app.use(express.json());
